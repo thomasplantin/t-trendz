cf08391873e7d80f9f57640f3ddcd724a834dade
thomasplantin <tplantin@utexas.edu>
2020-04-29 01:31:52 -0500

    Linked to local DB

--- claudia-ttrendz/index.js
+++ claudia-ttrendz/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 var path = require('path');
 const app = express();
-const morgan = require('morgan')
+const morgan = require('morgan');
+const mysql = require('mysql');
 
 app.use(morgan('short'))
 
@@ -9,26 +10,50 @@ const PORT = 8080;
 
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.get('/', function(req, res) {
-  console.log(__dirname);
+app.get('/', (req, res) => {
+  // Returns the root page of the website.
   res.sendFile('index.html');
 });
 
-app.get('/bracelets', function(req, res) {
-  console.log(__dirname);
-  res.sendFile(__dirname + '/public/jewelry/bracelets.html');
+app.get('/products/:category', (req, res) => {
+  /*
+  Fetches the products of the specified category from the DB 
+  and renders them on an html page.
+  */
+  const productCategory = req.params.category;
+  console.log('Fetching products from category: ' + productCategory);
+
+  const connection = mysql.createConnection( {
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 't_trendz_db'
+  });
+
+  const queryString = 'SELECT * FROM Products WHERE category = ?';
+  connection.query(queryString, [productCategory], (err, rows, fields) => {
+    if(err) {
+      console.log('Failed to query product - ERROR: ' + err);
+      res.sendStatus(500);
+      return;
+    }
+    res.json(rows);
+  });
+
 });
 
-app.get('/necklaces', function(req, res) {
-  console.log(__dirname);
-  res.sendFile(__dirname + '/public/jewelry/necklaces.html');
-});
+// app.get('/bracelets', (req, res) => {
+//   res.sendFile(__dirname + '/public/jewelry/bracelets.html');
+// });
 
-app.get('/earrings', function(req, res) {
-  console.log(__dirname);
-  res.sendFile(__dirname + '/public/jewelry/earrings.html');
-});
+// app.get('/necklaces', (req, res) => {
+//   res.sendFile(__dirname + '/public/jewelry/necklaces.html');
+// });
 
-app.listen(PORT, function () {
+// app.get('/earrings', (req, res) => {
+//   res.sendFile(__dirname + '/public/jewelry/earrings.html');
+// });
+
+app.listen(PORT, () => {
     console.log('[INFO] Listening on port ' + PORT + '...');
 });
