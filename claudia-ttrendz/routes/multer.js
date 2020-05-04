1ff7bd42c293de85d28a43eada9c386ce36237b1
thomasplantin <tplantin@utexas.edu>
2020-05-04 03:08:12 -0500

    Need to hash now

--- claudia-ttrendz/routes/multer.js
+++ claudia-ttrendz/routes/multer.js
@@ -2,10 +2,29 @@ const express = require('express');
 const multer = require('multer');
 const ejs = require('ejs');
 const path = require('path');
+const pbkdf2 = require('pbkdf2');
 
 const router = express.Router();
 
 // const options = {root: path.join(__dirname, '../views')};
+const {
+  ADMIN_USER,
+  ADMIN_PASS
+} = require('./../config.js');
+
+router.use((req, res, next) => {
+  const auth = {login: ADMIN_USER, password: ADMIN_PASS}; // change this
+  // parse login and password from headers
+  const b64auth = (req.headers.authorization || '').split(' ')[1] || '';
+  var [login, password] = Buffer.from(b64auth, 'base64').toString().split(':');
+  if (login && password && login === auth.login && password === auth.password) {
+    // Access granted...
+    return next();
+  }
+  // Access denied...
+  res.set('WWW-Authenticate', 'Basic realm="401"'); // change this
+  res.status(401).send('Authentication required.'); // custom message
+});
 
 // Set storage engine
 const storage = multer.diskStorage({
